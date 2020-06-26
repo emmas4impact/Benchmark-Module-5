afadb6e8b80ccfed18c1d29565bd557a812e1124
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-06-26 11:00:27 +0300

    First commit

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const listEndpoints = require("express-list-endpoints")
+const usersRouter = require("./service/users")
+const projectRouter = require("./service/project")
+const {join}= require("path")
+
+const cors = require("cors")
+
+
+const server = express()
+server.use(express.static(join(__dirname, `../public`)))
+
+const port = process.env.PORT || 3005
+
+const loggerMiddleware = (req, res, next) => {
+  console.log(`Logged ${req.url} ${req.method} -- ${new Date()}`)
+  next()
+}
+server.use(cors())
+server.use(express.json()) // Built in middleware
+server.use(loggerMiddleware)
+
+// ROUTES
+server.use("/users", loggerMiddleware, usersRouter)
+server.use("/project",loggerMiddleware, projectRouter)
+server.use("/problems", problematicRoutes)
+
+// ERROR HANDLERS
+
+
+console.log(listEndpoints(server))
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
